a279e96bfd4a2dbe6f5fab9478a52be32f78fe72
Sthephany Viana da Silva <sthephanyv021@gmail.com>
2025-12-01 21:52:31 -0300

    refactor: comment out DataLoader class and its methods for future reference

--- Projeto-Final-Tsuda/src/main/java/com/example/demo/DataLoader.java
+++ Projeto-Final-Tsuda/src/main/java/com/example/demo/DataLoader.java
@@ -1,51 +1,51 @@
-package com.example.demo;
+// package com.example.demo;
 
-import java.util.Date;
+// import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class DataLoader implements CommandLineRunner {
+// @Component
+// public class DataLoader implements CommandLineRunner {
 
-    @Autowired
-    private PublicacaoRepository publicacaoRepository;
+//     @Autowired
+//     private PublicacaoRepository publicacaoRepository;
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("=== INICIANDO DATALOADER ===");
+//     @Override
+//     public void run(String... args) throws Exception {
+//         System.out.println("=== INICIANDO DATALOADER ===");
         
-        long count = publicacaoRepository.count();
-        System.out.println("Quantidade de publicações no banco: " + count);
+//         long count = publicacaoRepository.count();
+//         System.out.println("Quantidade de publicações no banco: " + count);
         
-        // Só adiciona dados se estiver vazio
-        if (count == 0) {
-            System.out.println("Banco vazio! Carregando dados iniciais...");
+//         // Só adiciona dados se estiver vazio
+//         if (count == 0) {
+//             System.out.println("Banco vazio! Carregando dados iniciais...");
             
-            Publicacao pub1 = new Publicacao();
-            pub1.setTitulo("Como estudar programação");
-            pub1.setAutor("Sthephany Viana ");
-            pub1.setDataPublicacao(new Date());
-            pub1.setConteudo("Veja neste texto dicas de como programar");
-            pub1.setPublicado(true);
+//             Publicacao pub1 = new Publicacao();
+//             pub1.setTitulo("Como estudar programação");
+//             pub1.setAutor("Sthephany Viana ");
+//             pub1.setDataPublicacao(new Date());
+//             pub1.setConteudo("Veja neste texto dicas de como programar");
+//             pub1.setPublicado(true);
 
-            Publicacao pub2 = new Publicacao();
-            pub2.setTitulo("20 Dicas para falar bem");
-            pub2.setAutor("Ana Julia Ferrreira");
-            pub2.setDataPublicacao(new Date());
-            pub2.setConteudo("Aqui estão 20 dicas para você falar melhor em público");
-            pub2.setPublicado(false);
+//             Publicacao pub2 = new Publicacao();
+//             pub2.setTitulo("20 Dicas para falar bem");
+//             pub2.setAutor("Ana Julia Ferrreira");
+//             pub2.setDataPublicacao(new Date());
+//             pub2.setConteudo("Aqui estão 20 dicas para você falar melhor em público");
+//             pub2.setPublicado(false);
 
-            publicacaoRepository.save(pub1);
-            publicacaoRepository.save(pub2);
+//             publicacaoRepository.save(pub1);
+//             publicacaoRepository.save(pub2);
 
-            System.out.println(" Dados iniciais carregados no H2!");
-            System.out.println("Agora temos " + publicacaoRepository.count() + " publicações");
-        } else {
-            System.out.println("Banco já contém " + count + " publicações");
-        }
+//             System.out.println(" Dados iniciais carregados no H2!");
+//             System.out.println("Agora temos " + publicacaoRepository.count() + " publicações");
+//         } else {
+//             System.out.println("Banco já contém " + count + " publicações");
+//         }
         
-        System.out.println("=== DATALOADER FINALIZADO ===");
-    }
-}
+//         System.out.println("=== DATALOADER FINALIZADO ===");
+//     }
+// }
